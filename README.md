d177bc4e5224b375ce676b91825f3e1eb9e461c5
Lukasz Wisniewski <lwisniewski387@gmail.com>
2021-11-19 00:28:08 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# HTML Templates With Twig.js
+
+Supporting materials for a Youtube lesson: [Youtube](https://www.youtube.com/watch?v=gNS8hpmKK3w&ab_channel=CyberFountain)
+
+![image](https://cyberfountain.ams3.digitaloceanspaces.com/images/html-templates-with-twig-js.png)
---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+const gulp = require('gulp')
+const { src, series, dest, watch, task, parallel } = gulp
+
+const browsersync = require("browser-sync")
+const server = browsersync.create()
+const shell = require("gulp-shell")
+const sass = require('gulp-sass')(require('sass'))
+const sourcemaps = require('gulp-sourcemaps')
+const twig = require('gulp-twig')
+
+task('scss:dev', () => {
+    return src('src/assets/scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('public/assets/css'))
+})
+
+task('scss', () => {
+    return src('src/assets/scss/**/*.scss')
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(dest('public/assets/css'))
+})
+
+task('scss:watch', () => {
+    watch('src/assets/scss/**/*.scss', series(['scss:dev']))
+})
+
+task('twig', () => {
+    return src('src/views/**/[^_]*.twig')
+        .pipe(twig())
+        .pipe(dest('public'))
+})
+
+task('twig:watch', () => {
+    watch('src/views/**/*.twig', series(['twig']))
+})
+
+task('dev:serve', () => {
+    server.init({
+        files: ["public/**/*"],
+        watchEvents: ["add", "change", "addDir"],
+        server: "public",
+        port: 5000
+    })
+})
+
+task('public:clean', shell.task('npx rimraf public'))
+
+task('ts:compile', shell.task('npx rollup --config rollup.config.js'))
+task('ts:compile:dev', shell.task('npx rollup --config rollup.config.js --config-dev'))
+task('ts:watch', shell.task('npx rollup --config rollup.config.js --config-dev --watch'))
+
+task('build', series(['ts:compile', 'scss']))
+task('dev', series([
+    'public:clean',
+    parallel([
+        'scss:dev',
+        'twig',
+        'ts:compile:dev'
+    ]),
+    parallel([
+        'dev:serve',
+        'ts:watch',
+        'scss:watch',
+        'twig:watch'
+    ])
+]))
